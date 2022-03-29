8eb0acf5ee46457f1cd2bb408bfa1fdfbbb8d95b
zelfius <102620142+zelfius@users.noreply.github.com>
2022-03-29 20:32:59 +0200

    Add files via upload

---
+++ build/precache-manifest.a5f92fda38e10e694bc9732b0ad83b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "989d5f2498acadd9ec31",
+    "url": "/static/css/main.909a7301.chunk.css"
+  },
+  {
+    "revision": "989d5f2498acadd9ec31",
+    "url": "/static/js/main.989d5f24.chunk.js"
+  },
+  {
+    "revision": "64b03539bc06cdc05748",
+    "url": "/static/css/1.ac8ff277.chunk.css"
+  },
+  {
+    "revision": "64b03539bc06cdc05748",
+    "url": "/static/js/1.64b03539.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e79cf42bfb4b46e1e9d8d89c6d4e00c5",
+    "url": "/index.html"
+  }
+];
